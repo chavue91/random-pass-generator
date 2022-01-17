25c6de6bd57bdc1a1f8a3383ee614cd05091c9fa
Cha Vue <chavue09@gmail.com>
2022-01-16 21:51:38 -0600

    password generated

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var numberChar = "0123456789";
+var specialChar = "!#$%&()*+,-./:;<>=?@[]^_`|}{~";
+var pass = "";
+var randomNum = "";
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,6 +16,86 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+// Generating password
+var generatePassword = function() {
+  var passLength = window.prompt("Please choose a password length of 8-128 characters.");
+
+// Password length
+  if (passLength >= 8 && passLength <= 128) {
+    window.alert("You have chosen a password length of " + passLength + ".");
+  } 
+  else {
+    window.alert("Please enter a valid number between 8-128.");
+    return;
+  }
+  
+  // Uppercase letters
+  var upperCaseChoice = window.confirm("Would you like to include uppercase letters in your password?");
+
+  if (upperCaseChoice) {
+    window.alert("You have chosen to include uppercase letters in your password.");
+    pass = upperCaseChar += pass;
+  } 
+  else {
+    window.alert("You have chosen to not add upper case letters.");
+  }
+
+  //Lowercase letters
+  var lowerCaseChoice = window.confirm("Would you like to include lowercase letters in your password?");
+
+  if (lowerCaseChoice) {
+    window.alert("You have chosen to include lowercase letters in your password.");
+    pass = lowerCaseChar += pass;
+  } 
+  else {
+    window.alert("You have chosen to not include lower case letters.");
+  }
+
+  // Numeric
+  var numChoice = window.confirm("Would you like to include numbers in your password?");
+
+  if (numChoice) {
+    window.alert("You have chosen to include numbers in your password.");
+    pass = numberChar += pass;
+  } 
+  else {
+    window.alert("You have chosen to not include numbers.");
+  }
+
+  // Special characters
+  var specialCharChoice = window.confirm("Would you like to include special characters in your password?");
+  
+  if (specialCharChoice) {
+    window.alert("You have chosen to include special characters in your password.");
+    pass = specialChar += pass;
+  } 
+  else {
+    window.alert("You have chosen to not include special characters.");
+  }
+
+  //Choice confirmation
+  if (pass) {
+  window.alert("Generating your password...");
+  } 
+  else {
+    window.alert("Please select at least one of the 4 character options.");
+    return;
+  }
+
+  //Generate characters for password
+var generate = new Array();
+
+for (i = 0; i < passLength; i++) {
+  randomNum = Math.floor(Math.random() * pass.length);
+  arrayNum = pass[randomNum];
+  generate.push(arrayNum);
+}
+
+//Generate password
+password = generate.join( " " );
+return password; 
 }
 
 // Add event listener to generate button
